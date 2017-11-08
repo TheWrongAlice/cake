7302f364b00bb7a06af167cf3ec494018ea50015
Alice Cyan Carlsson <alice.cyan.carlsson@gmail.com>
2017-11-08 17:23:07 +0100

    Create cake.js

---
+++ cake.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var cakeUtilities = {
+isBestCake = function(type){
+return (type == 'chocolate cake');
+}
+};
+
+var myCake = {
+type: 'carrot cake',
+size: 8
+};
+
+console.log(cakeUtilities.isBestCake(myCake));
